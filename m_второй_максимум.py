2facf10605fad5ea515bb039556de1aeb7cc6607
Yernar111 <ernarsuinullaev1@gmail.com>
2025-01-30 21:34:04 +0500

    second add

--- m_второй_максимум.py
+++ m_второй_максимум.py
@@ -4,10 +4,11 @@ for i in range(n):
     t=int(input())
     a.append(t)
 
-k,m=0,0
+m,k=0,0
 for i in range(len(a)):
-    if a[i]%2==0:
-        m+=1
-    else:
-        k+=1
-print(f"Even: {m}",f"Odd: {k}")
+    if a[i]>m:
+        k=m
+        m=a[i]
+    elif a[i]>k and a[i]<m:
+        k=a[i]
+print(k)
